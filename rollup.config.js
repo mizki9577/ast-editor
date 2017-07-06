6f282f8a1e7dea719813a6363ba9920b9d0c30d5
Mizki SUZUMORI <mizki9577@gmail.com>
2017-07-07 00:35:58 +0900

    first commit

---
+++ rollup.config.js
@@ -0,0 +1,42 @@
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import babel from 'rollup-plugin-babel'
+import replace from 'rollup-plugin-replace'
+
+export default {
+  entry: 'src/main.js',
+  dest: 'dist/bundle.js',
+  format: 'es',
+  plugins: [
+    replace({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+    }),
+
+    resolve({
+      jsnext: true,
+    }),
+
+    commonjs(),
+
+    babel({
+      exclude: 'node_modules/**',
+      presets: [
+        'react',
+      ],
+      plugins: [
+        'external-helpers',
+      ],
+      env: {
+        'production': {
+          'presets': [
+            'babili',
+          ],
+        },
+      },
+    }),
+  ],
+}
+
+// vim: set ts=2 sw=2 et:
+
+
---
+++ src/main.js
@@ -0,0 +1,10 @@
+// @flow
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(
+  <div />,
+  document.getElementById('app')
+)
+
+// vim: set ts=2 sw=2 et:
