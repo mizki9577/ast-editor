1530f8f9d0c4a7acef869aa02b4ddd1471c7f767
Mizki SUZUMORI <mizki9577@gmail.com>
2017-08-22 18:17:31 +0900

    readable webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,49 @@
 const { DefinePlugin } = require('webpack')
 const BabiliPlugin = require("babili-webpack-plugin")
 
-module.exports = (env) => ({
-  entry: './src/renderer/main.js',
-
-  output: {
-    filename: 'bundle.js',
-  },
-
-  devtool: env.development ? 'inline-source-map' : false,
-  target: 'electron-renderer',
-
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              'react',
-              'stage-0',
-            ],
-          },
+module.exports = (env) => {
+  const rules = [
+    {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            'react',
+            'stage-0',
+          ],
         },
       },
+    },
+  ]
 
-    ],
-  },
-
-  plugins: [
+  const plugins = [
     new DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(env.production ? 'production' : 'development'),
     }),
-    ...(env.production ? [new BabiliPlugin()] : []),
-  ],
-})
+  ]
+
+  if (env.production) {
+    plugins.push(new BabiliPlugin())
+  }
+
+  return {
+    entry: './src/renderer/main.js',
+
+    output: {
+      filename: 'bundle.js',
+    },
+
+    devtool: env.development ? 'inline-source-map' : false,
+    target: 'electron-renderer',
+
+    module: {
+      rules,
+    },
+
+    plugins,
+  }
+}
 
 // vim: set ts=2 sw=2 et:
